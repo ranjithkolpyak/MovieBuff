b01b6d274b7c37c8f7a7bc6236c1eabcb1730690
Ranjith Reddy <rkolpyak@gmail.com>
2016-07-28 16:09:30 -0400

    feat(module-api): add configuration for db and initialize it

---
+++ server/MovieBuff-API/src/main/java/com/moviebuff/MongoConfig.java
@@ -0,0 +1,46 @@
+package com.moviebuff;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.MongoClient;
+
+@Configuration
+@EnableMongoRepositories
+public class MongoConfig extends AbstractMongoConfiguration{
+	
+	@Override
+	protected String getDatabaseName() {
+		return "examples";
+	}
+
+	@Override
+	@Bean
+	public MongoClient mongo() throws Exception {
+		return new MongoClient("localhost", 27017);
+	}
+
+	@Override
+	@Bean
+	protected String getMappingBasePackage() {
+		return "com.moviebuff.entities";
+	}
+
+	@Override
+	@Bean
+	public MongoTemplate mongoTemplate() throws Exception {
+		return new MongoTemplate(mongoDbFactory());
+	}
+
+	@Override
+	@Bean
+	public MongoDbFactory mongoDbFactory() throws Exception {
+		return new SimpleMongoDbFactory(mongo(), getDatabaseName());
+	}
+
+}
