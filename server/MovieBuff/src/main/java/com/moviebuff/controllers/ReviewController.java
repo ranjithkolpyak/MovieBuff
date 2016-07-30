e6569a0959957e94a2baddfbf5b42a1f527e0bc3
Ranjith Reddy <rkolpyak@gmail.com>
2016-07-30 17:30:56 -0400

    feat(module-api): use single controller for comment service and review service

---
+++ server/MovieBuff/src/main/java/com/moviebuff/controllers/ReviewController.java
@@ -0,0 +1,71 @@
+package com.moviebuff.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.moviebuff.entities.CommentDTO;
+import com.moviebuff.entities.ReviewDTO;
+import com.moviebuff.services.CommentService;
+import com.moviebuff.services.ReviewService;
+
+@RestController
+@RequestMapping("reviews")
+public class ReviewController {
+	
+	@Autowired
+	CommentService commentService;
+	
+	@Autowired
+	ReviewService reviewService;
+	
+	@RequestMapping(method=RequestMethod.POST, path="comments", consumes=MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public CommentDTO addComment(@RequestBody CommentDTO comment){
+		return commentService.addComment(comment);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, path="comments/flag/{commentId}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Boolean flagComment(@PathVariable("id") String commentId){
+		return commentService.flagComment(commentId);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, path="comments/{movieId}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public List<CommentDTO> getCommentsByMovie(@PathVariable("movieId") String movieId){
+		return commentService.getComments(movieId);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, path="comments/{commentId}")
+	public void deleteComment(@PathVariable("commentId") String commentId){
+		commentService.deleteComment(commentId);
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, path="ratings/{movieId}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ReviewDTO getReviews(@PathVariable("movieId") String movieId){
+		return reviewService.getReview(movieId);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, path="ratings", consumes=MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ReviewDTO updateReviews(@RequestBody ReviewDTO latest){
+		return reviewService.updateReviews(latest);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, path="ratings/add/{movieId}/{rating}", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public void addRating(@PathVariable("movieId") String movieId , @PathVariable("rating") Double rating){
+		reviewService.addRating(movieId, rating);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, path="ratings/add", consumes=MediaType.APPLICATION_JSON_UTF8_VALUE, produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ReviewDTO addReview(@RequestBody ReviewDTO latest){
+		return reviewService.addReview(latest);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, path="ratings/{reviewId}")
+	public void deleteReview(@PathVariable("reviewId") String reviewId){
+		reviewService.deleteReview(reviewId);
+	}
+	
+}
