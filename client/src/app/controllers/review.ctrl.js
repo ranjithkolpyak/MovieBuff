83c41d172bb5ad0cb2827876836961753a2f0cde
Ranjith Reddy <rkolpyak@gmail.com>
2016-08-15 00:28:33 -0400

    feat(module-complete): add review service controller and template

--- client/src/app/controllers/review.ctrl.js
+++ client/src/app/controllers/review.ctrl.js
@@ -1,3 +1,29 @@
 /**
  * Created by ranjithreddy on 8/13/16.
  */
+
+(function () {
+    'use strict';
+
+    angular.module('myApp')
+        .controller('ReviewController', ReviewController);
+
+    ReviewController.$inject = ['reviewService', '$routeParams'];
+    function ReviewController(reviewService, $routeParams) {
+
+        var reviewVm = this;
+
+        init();
+
+        function init() {
+            reviewService.getReviewByMovieId($routeParams.movieId)
+                .then(function (response) {
+                        reviewVm.review = response;
+                    },
+                    function (error) {
+                        console.log(error);
+                    });
+            console.log('reviewController');
+        }
+    }
+})();
--- client/src/app/services/reviewService.js
+++ client/src/app/services/reviewService.js
@@ -1,3 +1,33 @@
 /**
  * Created by ranjithreddy on 8/13/16.
  */
+
+
+(function () {
+    'use strict';
+
+    angular.module('myApp')
+        .service('reviewService', reviewService);
+
+    reviewService.$inject = ['$http', 'CONFIG', '$q'];
+    function reviewService($http, CONFIG, $q) {
+
+        var selfService = this;
+
+        selfService.getReviewByMovieId = getReviewByMovieId;
+
+        function getReviewByMovieId(id) {
+            console.log('ReviewService'+id);
+            return $http.get(CONFIG.API_HOST + '/reviews/'+ id)
+                .then(successFn, errorFn);
+        }
+
+        function successFn(response) {
+            return response.data;
+        }
+
+        function errorFn(response) {
+            return $q.reject('ERROR: ' + response.statusText);
+        }
+    }
+})();
